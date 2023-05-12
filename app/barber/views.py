4ff357f76b931715d1ac62811a6b55963d4a25c8
nguyenthainghia170402 <nguyenthainghia12b1ts2020@gmail.com>
2023-05-12 15:02:39 +0700

    Second Commit

--- app/barber/views.py
+++ app/barber/views.py
@@ -7,5 +7,37 @@ from datetime import datetime
 
 from . import barber
 from .. import db
-from ..models import Customer
+from ..models import Barber
+
+def check_admin():
+    """
+    Prevent non-admins from accessing the page
+    """
+    if not current_user.isadmin:
+        abort(403)
+
+@barber.route('/barbers', methods = ['GET'])
+@login_required
+def list_barbers():
+    check_admin()
+
+    barbers = Barber.query.filter_by(hide=False).all()
+    print(barbers, file=sys.stderr)
+    barbersList = []
+
+    for barber in barbers:
+        customerDict = {
+            "barberid" : barber.customerid,
+            "barbername" : barber.customername,
+            "phonenumber" : barber.phonenumber,
+            "address" : barber.address,
+            "birthday": barber.birthday,
+            "forte": barber.forte,
+            "description": barber.description,
+            "image": barber.image,
+            "createat" : barber.createat
+        }
+        barbersList.append(customerDict)
+
+    return jsonify({'customers' : barbersList})
 
